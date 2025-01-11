ab3897fb83f8de611eb73d4b275e5399b9b4885e
Jisto mj <jistojoy2020@gmail.com>
2025-01-11 16:35:38 +0530

    login page, signup page and email verfication page templates changed

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ const env = require('dotenv').config();
 const session = require("express-session");
 const passport = require('./config/passport');
 const db = require("./config/db");
-const userRouter = require('./routes/userRouter')
+const userRouter = require('./routes/userRouter');
+const adminRouter = require('./routes/adminRouter');
 db();
 
 app.use(express.json());
@@ -36,6 +37,7 @@ app.set("view engine", "ejs");
 app.set("views",[path.join(__dirname,'views/user'),path.join(__dirname,'views/admin')]);
 
 app.use("/",userRouter);
+app.use("/admin",adminRouter);
 
 app.listen(process.env.PORT, () => {
     console.log('Server running');
