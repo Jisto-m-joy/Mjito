33968f7350260d06e00af5bc52f908a20c2dbf8d
Jisto mj <jistojoy2020@gmail.com>
2025-03-18 09:37:10 +0530

    Again made some changes in the google authentication

--- config/passport.js
+++ config/passport.js
@@ -4,12 +4,14 @@ const User = require("../models/userSchema");
 const env = require("dotenv").config();
 
 passport.use(new GoogleStrategy({
-    clientID: process.env.GOOGLE_CLIENT_ID,
-    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: 'https://goalzone.ajaiii.tech/auth/google/callback'
+    clientID: '***REMOVED***',
+    clientSecret: '***REMOVED***',
+    callbackURL: 'https://goalzone.ajaiii.tech/auth/google/callback',
+    scope: ['profile', 'email']
 },
 async (accessToken, refreshToken, profile, done) => {
     try {
+        console.log('Google OAuth Profile:', profile); // Log the profile to debug
         let user = await User.findOne({ googleId: profile.id });
         if (user) {
             return done(null, user);
@@ -23,6 +25,7 @@ async (accessToken, refreshToken, profile, done) => {
             return done(null, user);
         }
     } catch (error) {
+        console.error('Google OAuth Error:', error); // Log the error
         return done(error, null);
     }
 }
